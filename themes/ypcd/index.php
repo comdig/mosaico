48c71a427acc6d37fe0dbff6fe75068d1979067b
Bruno Henrique Stein <brunohstein@gmail.com>
2012-07-29 17:07:46 -0300

    index.php first markup

---
+++ themes/ypcd/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+      <meta charset="utf-8" />
+        
+      <title></title>
+
+      <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+      <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+      <script type="text/javascript" src="jquery.instagram.js"></script>
+      <script type="text/javascript" src="jquery-1.7.2.min.js"></script>
+  </head>
+
+  <body>
+      <div class="instagram"></div>
+  </body>
+</html>
