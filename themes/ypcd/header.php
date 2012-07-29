8916cc31237ba789b869a3e666281e4dc368eb1d
Bruno Henrique Stein <brunohstein@gmail.com>
2012-07-29 19:02:35 -0300

    instagram api behavior

--- themes/ypcd/header.php
+++ themes/ypcd/header.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <title><?php wp_title(); ?></title>
-        <link rel="profile" href="http://gmpg.org/xfn/11" />
-        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-        <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-        <?php wp_head(); ?>
-        <meta charset="utf-8" />
-        <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/javascripts/jquery-1.7.2.min.js"></script>
-        <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/javascripts/jquery.instagram.js"></script>
-    </head>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+
+    <title><?php wp_title(); ?></title>
+
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+    <?php wp_head(); ?>
+    
+    <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/javascripts/jquery-1.7.2.min.js"></script>
+    <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/javascripts/jquery.instagram.js"></script>
+  </head>
--- themes/ypcd/index.php
+++ themes/ypcd/index.php
@@ -1,17 +1,41 @@
-<!DOCTYPE html>
-
-<html>
   <?php get_header( $name ); ?>
+
   <body>
-      <div class="instagram">
-	      <div class="instagram">
-		    <script type="text/javascript">
-			$(function() {
-			$(".instagram").instagram({
-			hash: 'love' , clientId: '9ea65159a89141ceab09c004b157c5cd'
-			});
-			});
-			</script>
-      </div>
+    <script type="text/javascript">
+      $(function(){
+
+        var insta_container = $(".instagram"), insta_next_url, i = 1;
+
+        function loadImages(){
+          insta_container.instagram({
+            next_url : insta_next_url,
+            show : 20,
+            onComplete : function(photos, data) {
+              insta_next_url = data.pagination.next_url, i++, loop()
+            }
+          })
+        };
+
+        insta_container.instagram({
+          hash: 'youpix',
+          clientId : '9ea65159a89141ceab09c004b157c5cd',
+          show : 20,
+          onComplete : function (photos, data) {
+            insta_next_url = data.pagination.next_url, loadImages()
+          }
+        });
+
+        function loop(){
+          if (i == 10) {
+            clearTimeout(loader);
+          } else {
+            var loader = setTimeout(loadImages(), 300);
+          }
+        }
+
+      });
+    </script>
+
+    <div class="instagram"></div>
   </body>
 </html>
